c7f7d510795dd5bf98443ab140f4ae66eee348c6
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2010-10-31 16:03:03 +0100

    Moving more info out of the "table" for showing.

--- static/js/schedule5.js
+++ static/js/schedule5.js
@@ -1,10 +1,27 @@
 jQuery(function() {
   window.scrollTo(0, 1);
+
   jQuery('nav a').click(function(event) {
     event.preventDefault();
     var offset = jQuery(this.hash).offset().top;
     jQuery(document.body).animate({scrollTop: offset},
       { duration: 400, easing: 'swing'}
     );
+  });
+
+  jQuery('.more-info-button.on').click(function(event) {
+    jQuery('#more-info-container')
+      .empty()
+      .append(jQuery('<div class="close-me" onclick=""></div>'))
+      .append(jQuery(this).siblings('.more-info').html())
+      .addClass('active');
+  });
+
+  jQuery('#more-info-container .close-me').live('click', function(event) {
+    var moreInfoButton = jQuery('#more-info-container');
+    moreInfoButton.addClass('closing');
+    moreInfoButton.one('webkitTransitionEnd', function(event) {
+      moreInfoButton.removeClass('active closing');
+    }, false);
   });
 });
